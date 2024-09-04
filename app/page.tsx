e5874db7c0d2ab25dd710903a247b949c7c2b297
Sompalkar <palkarsom1@gmail.com>
2024-09-04 22:56:21 +0530

    Updated:something

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@ import LandingPage from "@/components/LandingPage";
 import { Navbar } from "@/components/Navbar";
 export default function Home() {
   return (
-    <main className=" "> 
-      <Navbar/>
-
-      
+    <main className=" ">        
     
        <LandingPage/>
 
